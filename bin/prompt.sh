03c4fbda4db0d90770283eeb8f4a15561917d073
Nicholas Avenell <nicholas@aquarionics.com>
2013-01-07 03:23:00 +0000

    Initial Commit

---
+++ bin/prompt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+. /etc/bash_completion
+. ~/.bash_colours
+
+git_dirty_flag() {
+  return `git status 2> /dev/null | grep -c : | awk '{if ($1 > 0) printf "%s", "!"}'`
+}
+
+#parse_git_branch
+GIT_PS1_SHOWDIRTYSTATE=Yes
+GITSTATUS=`__git_ps1 | tr -d ' '`
+
+WD=$PWD
+SHORTWD=`echo $PWD | sed -e"s#$HOME#~#"`
+SHORTHOST=`hostname -s`;
+#GITSTATUS=`git status 2> /dev/null | grep -c : | awk '{if ($1 > 0) printf "%s", "!"}'`
+echo -en "$LIGHT_GREEN$SHORTHOST:$LIGHT_BLUE$SHORTWD$CYAN$GITSTATUS$NO_COLOUR"
