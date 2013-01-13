b58b65c71646197564328839b9bfcc2606ba0459
Nicholas Avenell <nicholas@aquarionics.com>
2013-01-13 23:31:35 +0000

    More useful command prompt

--- bin/prompt.sh
+++ bin/prompt.sh
@@ -15,4 +15,5 @@ WD=$PWD
 SHORTWD=`echo $PWD | sed -e"s#$HOME#~#"`
 SHORTHOST=`hostname -s`;
 #GITSTATUS=`git status 2> /dev/null | grep -c : | awk '{if ($1 > 0) printf "%s", "!"}'`
-echo -en "$LIGHT_GREEN$SHORTHOST:$LIGHT_BLUE$SHORTWD$CYAN$GITSTATUS$NO_COLOUR"
+#echo -en "$LIGHT_GREEN$SHORTHOST:$LIGHT_BLUE$SHORTWD$CYAN$GITSTATUS$NO_COLOUR"
+echo -en "$SHORTHOST:$SHORTWD$GITSTATUS"
