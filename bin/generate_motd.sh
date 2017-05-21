e234d98785a8afcf1a7f5c40468358483a3e7f07
Nicholas Avenell <nicholas@istic.net>
2017-05-21 06:36:04 +0200

    Generate MOTD

---
+++ bin/generate_motd.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo apt-get install -qqy figlet
+
+if [[ ! -f /usr/share/figlet/weird.flf ]]
+then
+	sudo su -c "curl -s http://www.figlet.org/fonts/weird.flf > /usr/share/figlet/weird.flf"
+fi
+
+cat << ENDDOC | sudo tee /etc/motd
+
+**************
+$(figlet `hostname -s` -f weird | sed "5s/$/.`hostname -d`/")
+**************
+
+`hostname` is owned and operated by Nicholas 'Aquarion' Avenell,
+he can be reached at nicholas@aquarionics.com, or on +447909 547 990
+
+ENDDOC
+
+
+if [[ -f /etc/motd.local ]]
+then
+	sudo su -c "cat /etc/motd.local >> /etc/motd"
+	cat /etc/motd.local
+fi
